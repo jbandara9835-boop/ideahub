75733c73362862a3bdcbed58aed9e79be8e9853a
jbandara9835-boop <jbandara9835@gmail.com>
2026-05-23 20:14:54 +0530

    Fix missing path import in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
