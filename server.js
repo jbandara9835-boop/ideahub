b86d8aa252af1820268df99942533986ca41dd5f
jbandara9835-boop <jbandara9835@gmail.com>
2026-05-26 01:11:35 +0530

    Add About and Contact pages, update nav links

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ const supabase = createClient(
 app.get('/discover', (req, res) => {
   res.sendFile(__dirname + '/public/discover.html');
 });
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
 app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/public/login.html');
 });
