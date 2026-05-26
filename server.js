347241e6274a71580ea3aba0d275c680a11512d9
jbandara9835-boop <jbandara9835@gmail.com>
2026-05-26 23:14:15 +0530

    Merge About and Contact into single page, free up nav slot

--- server.js
+++ server.js
@@ -54,9 +54,7 @@ app.get('/discover', (req, res) => {
 app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/public/about.html');
 });
-app.get('/contact', (req, res) => {
-  res.sendFile(__dirname + '/public/contact.html');
-});
+
 app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/public/login.html');
 });
